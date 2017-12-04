25c75625ad2dac7d54dd68cb84f852e29daea946
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-05 05:02:01 +0530

    Add http server instance to serve static files

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,31 @@
-const Server = require('ws').Server
-const sockServer = new Server({ port: 8080 })
+const express = require('express')
+const WebSocket = require('ws')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+let ws = new WebSocket.Server({server})
+app.use(express.static('../Client'))
 
-sockServer.on('connection', (ws) => {
-  ws.on('message', (msg) => {
+ws.on('connection', (connection, request) => {
+  console.log('Connected')
+  connection.on('message', (msg) => {
     console.log('Received: ' + msg)
-    ws.send(msg)
+    connection.send(msg)
+          // group chat
+    ws.clients.forEach((client) => {
+      if (client !== ws) client.send(msg)
+    })
   })
+  connection.on('close', () => {
+    console.log('Lost a client')
+  })
+  console.log('One more client connected')
+})
+
+ws.on('close', () => {
+  console.log('Closing web socket')
+})
+
+server.listen(8080, () => {
+  console.log('Example app listening on port 8080!')
 })
