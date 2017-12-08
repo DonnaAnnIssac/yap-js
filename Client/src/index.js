5f989b52681ff170db42b2e2bda65b123a62a32a
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-08 08:51:17 +0530

    Retrieve history and display

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,17 +1,22 @@
 let sock = new WebSocket('ws://localhost:8080')
 let friends = []
-let from
+let from, to
 let myId
 let recipient = document.getElementById('recipient')
 recipient.appendChild(document.createTextNode(''))
 let messages = document.getElementById('messages')
 let friendsList = document.getElementById('listFriends')
 
+document.getElementById('save').onclick = () => {
+  myId = document.getElementById('userName').value
+  sock.send(JSON.stringify(myId))
+}
 document.getElementById('ping').onclick = () => {
   let msgObj = {}
   msgObj['text'] = document.getElementById('text').value
-  messages.appendChild(document.createTextNode(msgObj['text']))
   msgObj['to'] = (from === undefined) ? recipient.textContent : from
+  msgObj['from'] = myId
+  messages.appendChild(document.createTextNode(msgObj['text']))
   sock.send(JSON.stringify(msgObj))
 }
 
@@ -24,18 +29,26 @@ sock.onerror = (error) => {
 }
 
 sock.onmessage = (event) => {
-  console.log('Message received from server')
-  if (typeof JSON.parse(event.data) === 'string') { // broadcast type msg
-    let msg = event.data.slice(1, event.data.length - 1)
-    messages = createChildWithText(messages, msg)
-  } else if (Array.isArray(JSON.parse(event.data))) { // client list
+  let data = JSON.parse(event.data)
+  if (typeof data === 'string') { // broadcast type msg
+    let msg = data.slice(1, data.length - 1)
+    displayChat(msg)
+  } else if (Array.isArray(data)) { // client list
     displayConnectedClients(event.data)
     addListenerToClient(friendsList)
-  } else if (typeof JSON.parse(event.data) === 'number') { // current client id
-    myId = JSON.parse(event.data)
+  } else if (data.history !== undefined) {
+    let chatHistory = data.history
+    let children = document.getElementsByClassName('msgs')
+    for (let i = 0; i < children.length; i++) messages.removeChild(children[i])
+    if (chatHistory.length !== 0) {
+      chatHistory.forEach((msg) => {
+        if ((msg.to === myId && msg.from === recipient.textContent) || (msg.to === recipient.textContent && msg.from === myId))
+          displayChat(msg)
+      })
+    }
   } else { // private message
-    messages = createChildWithText(messages, JSON.parse(event.data)['text'])
-    from = JSON.parse(event.data)['from']
+    displayChat(data)
+    from = data.from
     recipient.textContent = from
   }
 }
@@ -51,17 +64,27 @@ function displayConnectedClients (list) {
   friends = JSON.parse(list)
   friends.forEach((friend, i) => {
     if (!(friendsList.hasChildNodes()) || friendsList.childNodes.length - 1 < i) {
-      friendsList = createChildWithText(friendsList, friend)
+      friendsList = createChildWithText(friendsList, friend.name
+      )
     }
   })
 }
 
 function addListenerToClient (parent) {
-  // console.log('Adding listener')
   let children = parent.childNodes
   children.forEach((child) => {
     child.addEventListener('click', () => {
       recipient.textContent = child.textContent
+      sock.send(JSON.stringify({'to': recipient.textContent, 'from': myId}))
     })
   })
+}
+
+function displayChat (msg) {
+  let textDiv = document.createElement('div')
+  textDiv.className = 'msgs'
+  textDiv.appendChild(document.createTextNode(msg.text))
+  messages.appendChild(textDiv)
+  if (msg.from === myId) textDiv.style.alignSelf = 'flex-end'
+  else textDiv.style.alignSelf = 'flex-start'
 }
